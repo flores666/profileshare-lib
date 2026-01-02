2c1e69bf9c05104b3c315f44da932e15783455a1
flores666 <zatworld3@gmail.com>
2026-01-02 21:25:29 +0300

    fix: nil check

--- logger/sl.go
+++ logger/sl.go
@@ -3,6 +3,13 @@ package logger
 import "log/slog"
 
 func Error(err error) slog.Attr {
+	if err == nil {
+		return slog.Attr{
+			Key:   "error",
+			Value: slog.StringValue("unknown error"),
+		}
+	}
+
 	return slog.Attr{
 		Key:   "error",
 		Value: slog.StringValue(err.Error()),
